225a82a82e7d1b941bb5b1edc880040db3a5fc73
bouquety <abouquety1@gmail.com>
2021-12-28 00:10:15 +0100

    page d'accueil

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,26 @@
 <template>
-  <Tutorial/>
+  <div>
+
+    <div class="section_un">
+      <div class="container">
+                  <img src="../assets/images/Red and Black Shadow Loud Voices for Justice Logo (1).png">
+
+        <div class="row" id="champ_txt">
+          <h1 class="tittleone">ENVIE DE TESTER VOS CONNAISSANCES CINEMATOGRAPHIQUE ? </h1>
+          <p class="txt_title">
+            Bienvenue au quizz ! Une série de question vous sera proposé, vous devrez y répondre par "oui" ou par "non". Essayer de répondre au maximum de question que vous pouvez dans le temps imparti ! Bonne chance !
+          </p>
+        </div>
+        <nuxt-link to="/quizz"><b-button class="btn_start">Go</b-button></nuxt-link>
+      </div>
+    </div>
+
+   
+    
+
+    
+      
+  </div>
 </template>
 
 <script>
@@ -7,3 +28,60 @@ export default {
   name: 'IndexPage'
 }
 </script>
+<style scoped>
+
+
+img{
+      position: absolute;
+    left: 41%;
+    top: 18%;
+}
+.section_un {
+  background-image: url("../assets/images/black-monochrome-simple-background-texture-circle-light-534895-wallhere.com.jpg");
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-size: cover;
+  background-attachment: fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  image-rendering: -webkit-optimize-contrast;
+}
+
+.tittleone {
+  color: #fff;
+    font-size: 35px;
+    text-transform: uppercase;
+    font-weight: bold;
+    padding-bottom: 19px;
+    margin-top: 30px;
+}
+
+.txt_title {
+  color: #fff;
+  font-size: 20px;
+  line-height: 1.3;
+}
+
+.btn_start {
+  background-color: #e25d5d;
+  border-radius: 0px;
+  color: rgb(0, 0, 0);
+  font-size: 18px;
+  padding: 10px 40px;
+  margin-top: 30px;
+  margin-left: 510px;
+}
+.btn_start:hover {
+  background-color: #fff;
+  color: #555;
+  border: 1px solid #fff;
+}
+
+#champ_txt {
+  margin-top: 154px;
+  margin-left: 290px;
+}
+</style>
---
+++ pages/quizz/index.vue
@@ -0,0 +1,51 @@
+<template>
+ <div class="section_un">
+      <div class="container">
+
+        
+      </div>
+    </div>
+</template>
+
+<script>
+export default{
+    data() {
+    this.apikeys= process.env.APIKEY
+    return{
+        baseUrl : 'https://api.themoviedb.org/3/discover/movie',
+        movie:{},
+        movie_title:[]
+    }
+    },
+  mounted(){
+      this.initQuizz()
+  },
+
+    methods: {
+        async initQuizz(){
+            this.movie = await this.$axios.$get(this.baseUrl+'?api_key='+this.apikeys)
+            for (var i = 0; i < this.movie.results.length; i++) {
+                this.movie_title.push(this.movie.results[i].title)
+                }
+        }
+    }
+}
+</script>
+<style scoped>
+.container_header {
+  margin: 0px;
+}
+.section_un {
+  background-image: url("../../assets/images/black-monochrome-simple-background-texture-circle-light-534895-wallhere.com.jpg");
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-size: cover;
+  background-attachment: fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  image-rendering: -webkit-optimize-contrast;
+}
+</style>
